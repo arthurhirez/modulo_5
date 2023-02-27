a5d6a10c812eda47c07dbcb0a346cd0b03d45dcf
Arthur HR <arthurhr@usp.br>
2023-02-27 20:25:58 -0300

    1st commit

--- modulo/calculadora.py
+++ modulo/calculadora.py
@@ -1,5 +1,44 @@
+<<<<<<< HEAD
 def somar(x, y):
     return (x + y)
 
 def subtracao(x,y):
     return (x - y)
+=======
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "0ded0c2d-d53c-4e9e-9652-ef911e381ed8",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def somar(x,y):\n",
+    "    return x + y"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
+>>>>>>> parent of 4a50a8f (2nd version)
